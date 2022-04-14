43f33a12ad5d7b08dbd9c7ca7307021a67a1a6ee
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-14 14:24:39 -0500

    Add debounce with pin as INPUT_PULLUP

--- pushbutton/pushbutton.ino
+++ pushbutton/pushbutton.ino
@@ -1,46 +1,86 @@
 /*
  * Name: push-button.ino
  * Description: Example of using a momentary digital push button switch on a
- * pin that is set as an input pullup.
+ * pin that is set as an input pullup. With debounce of the push button.
  * */
-#define BAUD 9600
 
-byte pinFour = 4;
+const byte ledPin = 13;
+const byte buttonPin = 4;
 
-uint32_t ledClk = millis();
-const uint32_t ledPer = 1000;
+byte ledState = LOW;
+byte buttonState = HIGH;
+byte lastButtonState = HIGH;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 20;
+
+unsigned long foo = millis();
+unsigned long bar = 500;
 
 void setup()
 {
-#ifdef BAUD
-  Serial.begin(BAUD);
-#endif
+  Serial.begin(9600);
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(pinFour, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT_PULLUP);
+
+  digitalWrite(ledPin, ledState);
 }
 
 void loop()
 {
-  byte four = digitalRead(pinFour);
+  byte reading = digitalRead(buttonPin);
 
-  if (four)
-    digitalWrite(LED_BUILTIN, !HIGH);
-  else if (!(four))
-    digitalWrite(LED_BUILTIN, HIGH);
+  if (reading != lastButtonState)
+  {
+    lastDebounceTime = millis();
+    Serial.print("lDT: ");
+    Serial.println(lastDebounceTime);
+  }
 
-#ifdef BAUD
-  Serial.println(four);
-#endif
+  if (millis() - lastDebounceTime > debounceDelay)
+  {
+    Serial.print("millis() - lDT > dbD: ");
+    Serial.println(millis() - lastDebounceTime);
+    if (reading != buttonState)
+    {
+      buttonState = reading;
+      Serial.print("bS: ");
+      Serial.println(buttonState);
 
-  // flashLed(millis(), &ledClk, ledPer, LED_BUILTIN);
+      if (buttonState == LOW)
+      {
+        ledState = !ledState;
+        Serial.print("lS: ");
+        Serial.println(ledState);
+      }
+    }
+  }
+
+  digitalWrite(ledPin, ledState);
+
+  lastButtonState = reading;
+
+  // if (millis() - foo >= bar)
+  // {
+  //   printVals(reading);
+  //   foo = millis();
+  // }
 }
 
-void flashLed(uint32_t ms, uint32_t *clk, uint32_t per, uint8_t pin)
+void printVals(byte readPin)
 {
-  if (ms - *clk >= per)
-  {
-    digitalWrite(pin, !digitalRead(pin));
-    *clk = ms;
-  }
+  Serial.print("ledSta: ");
+  Serial.println(ledState);
+  Serial.print("butSta: ");
+  Serial.println(buttonState);
+  Serial.print("lButSt: ");
+  Serial.println(lastButtonState);
+  Serial.print("lDbTme: ");
+  Serial.println(lastDebounceTime);
+  Serial.print("dbDlay: ");
+  Serial.println(debounceDelay);
+  Serial.print("readin: ");
+  Serial.println(readPin);
+  Serial.println();
 }
