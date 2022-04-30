ef52f39b23a8004673b22402a81211e3d77c8830
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-30 11:51:16 -0500

    Add led flash clock and period per flash variables

--- interrupts/interrupts.ino
+++ interrupts/interrupts.ino
@@ -6,9 +6,17 @@
 
 const byte ledPin = 13;
 const byte interruptPin2 = 2;
-const byte nterruptPin3 = 3;
+const byte interruptPin3 = 3;
 const byte ledPin4 = 4;
 const byte ledPin5 = 5;
+
+const unsigned long led13Per = 333;
+const unsigned long led4Per = 3000;
+const unsigned long led5Per = 10000;
+
+unsigned long led13Clk = millis();
+unsigned long led4Clk = millis();
+unsigned long led5Clk = millis();
 
 volatile bool flag2 = false;
 volatile bool flag3 = false;
