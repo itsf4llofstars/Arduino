54ce6bc5e46331144e01d2353d2d3f65a190e34a
itsf4llofstars <irooted4hal@mailfence.com>
2022-05-03 12:02:30 -0500

    Dead foot code written
    Held at end of set until pulsePin is un-grounded

---
+++ deadfoot/deadfoot.ino
@@ -0,0 +1,58 @@
+/*
+ * The dead foot switch.
+ * This piece of code can be used to hold your arduino at the end of the
+ * setup function until to through a switch or remove a wire from a pin. We
+ * will look at the latter. This is good to prevent the loop from running
+ * until
+ * */
+
+// #define BAUD 9600
+
+const uint8_t touchPin = 12;
+const uint8_t touchPinLed = 11;
+const uint8_t pulsePin = 2;
+
+const uint32_t ledPer = 1000;
+uint32_t ledClk = millis();
+
+void setup() {
+#ifdef BAUD
+  Serial.begin(BAUD);
+#endif
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(touchPin, INPUT_PULLUP);
+  pinMode(touchPinLed, OUTPUT);
+  pinMode(pulsePin, INPUT_PULLUP);
+
+  // dead foot code
+  digitalWrite(touchPinLed, HIGH);
+
+  do {
+  } while (!pulseIn(pulsePin, HIGH, 1000));
+
+  digitalWrite(touchPinLed, LOW);
+}
+
+void loop() {
+  uint8_t touchPinVal = digitalRead(touchPin);
+  uint8_t foo = digitalRead(pulsePin);
+
+ if (touchPinVal)
+   digitalWrite(touchPinLed, HIGH);
+ else if (!touchPinVal)
+   digitalWrite(touchPinLed, LOW);
+
+  flashLed(millis(), &ledClk, ledPer, LED_BUILTIN);
+
+#ifdef BAUD
+  Serial.println(touchPinVal);
+#endif
+}
+
+void flashLed(uint32_t mils, uint32_t *clk, uint32_t per, uint8_t pin) {
+  if (mils - *clk >= per) {
+    *clk = mils;
+    digitalWrite(pin, !digitalRead(pin));
+  }
+}
